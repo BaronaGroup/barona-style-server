66cd2a49b829d3c8bca0f810947effff106190fa
Tapani Haka <tapani.haka@barona.fi>
2021-08-25 11:14:39 +0300

    Add upgrade suggestion

--- bin/run.sh
+++ bin/run.sh
@@ -5,3 +5,7 @@ if [ -e node_modules/eslint ]
 then
   $(dirname $0/)/eslint-staged-files.sh
 fi
+
+echo "*** Please update to a new alternative by running ***"
+echo
+echo "   npx pre-commit-eslint-and-prettier --full-install"
